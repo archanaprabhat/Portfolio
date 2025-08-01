b94a9a585e5cca0928225b5de114105cdb1011e9
archanaprabhat <archanaprabhath2002@gmail.com>
2025-08-01 21:56:31 +0530

    Add smooth scrolling to HTML and include About component in Home page

---
+++ app/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about' className='w-full px-[12%] py-10 scroll mt-20'>
+        <h4>Intro.js</h4>
+        <h2>me.md</h2>
+    </div>
+  )
+}
+
+export default About
