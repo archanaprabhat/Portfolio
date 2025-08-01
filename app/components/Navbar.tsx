696f9fb7d91bdacfccdc4123fc7d99c8dcb235ec
archanaprabhat <archanaprabhath2002@gmail.com>
2025-08-01 14:40:40 +0530

    Update Navbar component: adjust logo size, rename 'About' to 'Journey', and modify navigation links for improved clarity and layout.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,19 +7,18 @@ const Navbar = () => {
     <>
     <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex justify-between items-center z-50'>
         <a href="#top">
-            <Image src={assets.logo} alt="logo" className='w-20 h-20 cursor-pointer mb-14' />
+            <Image src={assets.logo} alt="logo" className='w-30 cursor-pointer ' />
         </a>
 
-        <ul>
+        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
             <li><a href="#top">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="#about">Journey</a></li>
             <li><a href="#skills">Skills</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#builds">Builds</a></li>
         </ul>
 
         <div>
-            <a href="#contact">Contact <Image src={assets.arrow_icon} alt="contact" className='w-3 cursor-pointer' /></a>
+            <a href="#contact" className='hidden lg:flex items-center gap-3 px-8 py-2.5 border border-gray-400 rounded-full ml-4'>Connect <Image src={assets.arrow_icon} alt="contact" className='w-3 cursor-pointer' /></a>
         </div>
     </nav>
 
