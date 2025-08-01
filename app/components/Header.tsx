682b02f8c3d669cc26caa1474006ae5d6e9b9290
archanaprabhat <archanaprabhath2002@gmail.com>
2025-08-01 21:47:29 +0530

    Update Header component: correct text capitalization, improve greeting message, and enhance overall readability.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,8 +7,8 @@ const Header = () => {
         <div>
             <Image src={assets.profile_img} alt='' className="rounded-full  w-32"/>
         </div>
-        <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">Hey hooman 👽 I’m Archana <Image src={assets.hand_icon} alt="" className="w-6"/> </h3>
-        <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">Frontend Developer</h1>
+        <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">Hey hooman, I’m Archana <Image src={assets.hand_icon} alt="" className="w-6"/> </h3>
+        <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">frontend developer</h1>
         <p>Frontend developer with 2 years of experience. Been in backend, but frontend’s what keeps me obsessed</p>
         <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
             <a href="#contact" className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2">Let's Connect <Image src={assets.right_arrow_white} alt='' className=" w-4"/></a>
